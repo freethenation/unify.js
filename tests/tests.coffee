ef7ccefd5e33a96746416a0007f6fb2eeafa7291
freethenation <rpklafter@yahoo.com>
2012-12-11 21:32:25 -0600

    1) Added a bunch of unit tests

--- tests/tests.coffee
+++ tests/tests.coffee
@@ -1,23 +1,29 @@
+#General Util Functions
 str=(obj)->
     if obj == null then "null"
     else if typeof obj == "undefined" then "undefined"
     else obj.toString()
 
+#General Testing Code
 class Test
     constructor:(@name, @func)->
         @num = 0
     expect:(num)->
         @num = num
-    equal:(arg1, arg2)->
+    equal:(arg1, arg2, message="''")->
         @num--
-        if arg1 != arg2 then throw "'#{str(arg1)}' does not equal '#{str(arg2)}'"
-    ok:(bool)->
+        if arg1 != arg2 then throw "'#NotEqual: {str(arg1)}' does not equal '#{str(arg2)}\n   #{message}'"
+    deepEqual:(arg1, arg2, message="")->
         @num--
-        if not bool then throw "false was passed to ok"
-    done:()->
-        if @num != 0 then throw "#{str(@num)} more checks were expected before done was called"
+        if not require('deep-equal')(arg1, arg2) then throw "'#NotEqual: {str(arg1)}' does not equal '#{str(arg2)}\n   #{message}"
+    ok:(bool,message="")->
+        @num--
+        if not bool then throw "NotOk: false was passed to ok\n   #{message}"
+    done:(message="")->
+        if @num != 0 then throw "NotDone: #{str(@num)} more checks were expected before done was called\n   #{message}"
     run:()->
         @func.call(this)
+        @done()
         
 test=(name, func)->
     t = new Test(name, func)
@@ -30,16 +36,129 @@ exports.RunAll = ()->
                 exports[name]()
             catch ex
                 console.log "Error in Test '#{name}'"
-                console.log ex
+                console.log "Message: #{ex}"
+                console.log "Stack:\n#{ex.stack}"
                 console.log ''
     return
 
-unify=require("../lib/unify")
+#File specific test functions
+unifylib=require("../lib/unify")
+for prop of unifylib
+    global[prop] = unifylib[prop]
 
-#test "Basic", ()->
-#    steps = []
-#    steps.push (step, err)=>
-#        callMeBack(step.next, 1, 2, 3)
-#    steps.push (step, err)=>
-#        callMeBack(step.next)
-#    funcflow(steps, @done)
+class UnifyTest extends Test
+    parsetest:(obj) ->
+        @num++
+        @deepEqual(parse(obj).unparse(),obj, "parse")
+    unifytest:(obj1, obj2) -> 
+        @num++
+        @ok(unify(obj1, obj2), "unify")
+    unifyfailtest:(obj1, obj2) ->
+        @num++
+        @ok(!unify(obj1,obj2), "unify fail")
+    gettest:(tin, varValueDict) ->
+        for v of varValueDict
+            @num++
+            if varValueDict[v] instanceof variable
+                @ok(tin.get(v) instanceof variable, "get(#{ v }) = variable()")
+            else
+                @deepEqual(tin.get(v), varValueDict[v], "get(#{ v }) == #{ toJson varValueDict[v] }")
+    fulltest:(obj1, obj2, varValueDict1, varValueDict2) ->
+        @parsetest(obj1)
+        @parsetest(obj2)
+        obj1 = parse(obj1)
+        obj2 = parse(obj2)
+        @unifytest(obj1, obj2)
+        @gettest(obj1, varValueDict1)
+        @gettest(obj2, varValueDict2)
+
+test=(name, func)->
+    t = new UnifyTest(name, func)
+    exports[name]=()->t.run()  
+ 
+#######################
+#full tests
+#######################
+test "empty obj {} -> {}", ()->
+    @fulltest({}, {}, {}, {})
+test "null test [null] -> [null]", ()->
+    @fulltest([null], [null], {}, {})
+test "variable equal [X] -> [1]", ()->
+    @fulltest([variable("a")], [1], {a:1}, {})
+test "variable equal [X,X] -> [1,1]", ()->
+    @fulltest([variable("a"), variable("a")], [1,1], {a:1}, {})
+test "variable equal [[1,2,3]] -> [y]", ()->
+    @fulltest([[1,2,3]], [variable("y")], {}, {y:[1,2,3]})
+test "variable equal [[1,2,x],x] -> [y,3]", ()->
+    @fulltest(
+        [[1,2,variable("x")],variable("x")],
+        [variable("y"),3],
+        {x:3}, {y:[1,2,3]})
+test "unbound variable [y]->[x]", ()->
+    @fulltest([variable("y")], [variable("x")], {y:variable("x")}, {x:variable("x")})
+test "variable equal [1,X,X] -> [Z,Z,1]", () ->
+    @fulltest([1, variable("X"), variable("X")], [variable("Z"), variable("Z"), 1], {X:1}, {Z:1})
+#######################
+#unify fail tests
+#######################
+test "variable equal [X,X] -> [1,2]", ()->
+    @unifyfailtest([variable("a"), variable("a")], [1,2])
+test "variable unequal [1,3,2] -> [Y,Y,2]", () ->
+    @unifyfailtest([1, 3, 2], [variable("y"), variable("y"), 2])
+test "variable unequal [1,X,X] -> [Z,Z,3]", () ->
+    @unifyfailtest([1, variable("X"), variable("X")], [variable("Z"), variable("Z"), 3])
+#######################
+#misc tests
+#######################
+test "simple black box unify test", () ->
+    @expect(1)
+    @ok(unify({a: [1,2,3]}, {a: [1,variable("b"),3]}))
+#######################
+#unify tests
+#######################
+test "variable equal [X,2,X] -> [1,2,1]", () ->
+    @expect(2)
+    tins = unify([variable("x"), 2, variable("x")], [1,2,1])
+    @ok(tins)
+    @deepEqual(tins[0].get_all(), {"x":1})
+#######################
+#extract tests
+#######################
+test "simple variable extraction test", () ->
+    @expect(1)
+    tins = unify({a: [1,2,3]}, {a: [1,variable("b"),3]})
+    @ok(tins[1].get("b") == 2)
+test "extract all variables test", () ->
+    @expect(1)
+    tins = unify({a: [1,2,3]}, {a: [1,variable("b"),3]})
+    @deepEqual(tins[1].get_all(), {"b":2})
+#######################
+#hidden variables tests
+#######################
+test "create hidden variable", () ->
+    @expect(1)
+    @ok((variable("_")).isHiddenVar())
+test "simple hidden variable [_,X] -> [1,2]", () ->
+    @fulltest([variable("_"),variable("x")],[1,2],{"x":2},{})
+test "multiple hidden variables [_,_,X] -> [1,2,3]", () ->
+    @fulltest([variable("_"),variable("_"),variable("x")],[1,2,3],{"x":3},{})
+test "[[1,_,3],[1,2,3]] -> [X,X]", () ->
+    @fulltest([[1,variable("_"),3],[1,2,3]],[variable("x"),variable("x")],{},{"x":[1,2,3]})
+#######################
+#rollback tests
+#######################
+test "rollback successful unification", () ->
+    @expect(3)
+    obj1 = [1,2,3]
+    obj2 = [variable("A"), variable("B"), 3]
+    @parsetest(obj1)
+    @parsetest(obj2)
+    obj1 = parse(obj1)
+    obj2 = parse(obj2)
+    cobj1 = eval(obj1.toString())
+    cobj2 = eval(obj2.toString())
+    changes = []
+    @ok(unify(obj1, obj2, changes), "unify")
+    rollback(changes)
+    @ok( obj1.toString() == cobj1.toString() )
+    @ok( obj2.toString() == cobj2.toString() )
--- tests/tests.js
+++ tests/tests.js
@@ -1,5 +1,7 @@
 (function() {
-  var Test, str, test, unify;
+  var Test, UnifyTest, prop, str, test, unifylib,
+    __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
   str = function(obj) {
     if (obj === null) {
@@ -23,28 +25,48 @@
       return this.num = num;
     };
 
-    Test.prototype.equal = function(arg1, arg2) {
+    Test.prototype.equal = function(arg1, arg2, message) {
+      if (message == null) {
+        message = "''";
+      }
       this.num--;
       if (arg1 !== arg2) {
-        throw "'" + (str(arg1)) + "' does not equal '" + (str(arg2)) + "'";
+        throw "'#NotEqual: {str(arg1)}' does not equal '" + (str(arg2)) + "\n   " + message + "'";
       }
     };
 
-    Test.prototype.ok = function(bool) {
+    Test.prototype.deepEqual = function(arg1, arg2, message) {
+      if (message == null) {
+        message = "";
+      }
+      this.num--;
+      if (!require('deep-equal')(arg1, arg2)) {
+        throw "'#NotEqual: {str(arg1)}' does not equal '" + (str(arg2)) + "\n   " + message;
+      }
+    };
+
+    Test.prototype.ok = function(bool, message) {
+      if (message == null) {
+        message = "";
+      }
       this.num--;
       if (!bool) {
-        throw "false was passed to ok";
+        throw "NotOk: false was passed to ok\n   " + message;
       }
     };
 
-    Test.prototype.done = function() {
+    Test.prototype.done = function(message) {
+      if (message == null) {
+        message = "";
+      }
       if (this.num !== 0) {
-        throw "" + (str(this.num)) + " more checks were expected before done was called";
+        throw "NotDone: " + (str(this.num)) + " more checks were expected before done was called\n   " + message;
       }
     };
 
     Test.prototype.run = function() {
-      return this.func.call(this);
+      this.func.call(this);
+      return this.done();
     };
 
     return Test;
@@ -67,13 +89,223 @@
           exports[name]();
         } catch (ex) {
           console.log("Error in Test '" + name + "'");
-          console.log(ex);
+          console.log("Message: " + ex);
+          console.log("Stack:\n" + ex.stack);
           console.log('');
         }
       }
     }
   };
 
-  unify = require("../lib/unify");
+  unifylib = require("../lib/unify");
+
+  for (prop in unifylib) {
+    global[prop] = unifylib[prop];
+  }
+
+  UnifyTest = (function(_super) {
+
+    __extends(UnifyTest, _super);
+
+    function UnifyTest() {
+      return UnifyTest.__super__.constructor.apply(this, arguments);
+    }
+
+    UnifyTest.prototype.parsetest = function(obj) {
+      this.num++;
+      return this.deepEqual(parse(obj).unparse(), obj, "parse");
+    };
+
+    UnifyTest.prototype.unifytest = function(obj1, obj2) {
+      this.num++;
+      return this.ok(unify(obj1, obj2), "unify");
+    };
+
+    UnifyTest.prototype.unifyfailtest = function(obj1, obj2) {
+      this.num++;
+      return this.ok(!unify(obj1, obj2), "unify fail");
+    };
+
+    UnifyTest.prototype.gettest = function(tin, varValueDict) {
+      var v, _results;
+      _results = [];
+      for (v in varValueDict) {
+        this.num++;
+        if (varValueDict[v] instanceof variable) {
+          _results.push(this.ok(tin.get(v) instanceof variable, "get(" + v + ") = variable()"));
+        } else {
+          _results.push(this.deepEqual(tin.get(v), varValueDict[v], "get(" + v + ") == " + (toJson(varValueDict[v]))));
+        }
+      }
+      return _results;
+    };
+
+    UnifyTest.prototype.fulltest = function(obj1, obj2, varValueDict1, varValueDict2) {
+      this.parsetest(obj1);
+      this.parsetest(obj2);
+      obj1 = parse(obj1);
+      obj2 = parse(obj2);
+      this.unifytest(obj1, obj2);
+      this.gettest(obj1, varValueDict1);
+      return this.gettest(obj2, varValueDict2);
+    };
+
+    return UnifyTest;
+
+  })(Test);
+
+  test = function(name, func) {
+    var t;
+    t = new UnifyTest(name, func);
+    return exports[name] = function() {
+      return t.run();
+    };
+  };
+
+  test("empty obj {} -> {}", function() {
+    return this.fulltest({}, {}, {}, {});
+  });
+
+  test("null test [null] -> [null]", function() {
+    return this.fulltest([null], [null], {}, {});
+  });
+
+  test("variable equal [X] -> [1]", function() {
+    return this.fulltest([variable("a")], [1], {
+      a: 1
+    }, {});
+  });
+
+  test("variable equal [X,X] -> [1,1]", function() {
+    return this.fulltest([variable("a"), variable("a")], [1, 1], {
+      a: 1
+    }, {});
+  });
+
+  test("variable equal [[1,2,3]] -> [y]", function() {
+    return this.fulltest([[1, 2, 3]], [variable("y")], {}, {
+      y: [1, 2, 3]
+    });
+  });
+
+  test("variable equal [[1,2,x],x] -> [y,3]", function() {
+    return this.fulltest([[1, 2, variable("x")], variable("x")], [variable("y"), 3], {
+      x: 3
+    }, {
+      y: [1, 2, 3]
+    });
+  });
+
+  test("unbound variable [y]->[x]", function() {
+    return this.fulltest([variable("y")], [variable("x")], {
+      y: variable("x")
+    }, {
+      x: variable("x")
+    });
+  });
+
+  test("variable equal [1,X,X] -> [Z,Z,1]", function() {
+    return this.fulltest([1, variable("X"), variable("X")], [variable("Z"), variable("Z"), 1], {
+      X: 1
+    }, {
+      Z: 1
+    });
+  });
+
+  test("variable equal [X,X] -> [1,2]", function() {
+    return this.unifyfailtest([variable("a"), variable("a")], [1, 2]);
+  });
+
+  test("variable unequal [1,3,2] -> [Y,Y,2]", function() {
+    return this.unifyfailtest([1, 3, 2], [variable("y"), variable("y"), 2]);
+  });
+
+  test("variable unequal [1,X,X] -> [Z,Z,3]", function() {
+    return this.unifyfailtest([1, variable("X"), variable("X")], [variable("Z"), variable("Z"), 3]);
+  });
+
+  test("simple black box unify test", function() {
+    this.expect(1);
+    return this.ok(unify({
+      a: [1, 2, 3]
+    }, {
+      a: [1, variable("b"), 3]
+    }));
+  });
+
+  test("variable equal [X,2,X] -> [1,2,1]", function() {
+    var tins;
+    this.expect(2);
+    tins = unify([variable("x"), 2, variable("x")], [1, 2, 1]);
+    this.ok(tins);
+    return this.deepEqual(tins[0].get_all(), {
+      "x": 1
+    });
+  });
+
+  test("simple variable extraction test", function() {
+    var tins;
+    this.expect(1);
+    tins = unify({
+      a: [1, 2, 3]
+    }, {
+      a: [1, variable("b"), 3]
+    });
+    return this.ok(tins[1].get("b") === 2);
+  });
+
+  test("extract all variables test", function() {
+    var tins;
+    this.expect(1);
+    tins = unify({
+      a: [1, 2, 3]
+    }, {
+      a: [1, variable("b"), 3]
+    });
+    return this.deepEqual(tins[1].get_all(), {
+      "b": 2
+    });
+  });
+
+  test("create hidden variable", function() {
+    this.expect(1);
+    return this.ok((variable("_")).isHiddenVar());
+  });
+
+  test("simple hidden variable [_,X] -> [1,2]", function() {
+    return this.fulltest([variable("_"), variable("x")], [1, 2], {
+      "x": 2
+    }, {});
+  });
+
+  test("multiple hidden variables [_,_,X] -> [1,2,3]", function() {
+    return this.fulltest([variable("_"), variable("_"), variable("x")], [1, 2, 3], {
+      "x": 3
+    }, {});
+  });
+
+  test("[[1,_,3],[1,2,3]] -> [X,X]", function() {
+    return this.fulltest([[1, variable("_"), 3], [1, 2, 3]], [variable("x"), variable("x")], {}, {
+      "x": [1, 2, 3]
+    });
+  });
+
+  test("rollback successful unification", function() {
+    var changes, cobj1, cobj2, obj1, obj2;
+    this.expect(3);
+    obj1 = [1, 2, 3];
+    obj2 = [variable("A"), variable("B"), 3];
+    this.parsetest(obj1);
+    this.parsetest(obj2);
+    obj1 = parse(obj1);
+    obj2 = parse(obj2);
+    cobj1 = eval(obj1.toString());
+    cobj2 = eval(obj2.toString());
+    changes = [];
+    this.ok(unify(obj1, obj2, changes), "unify");
+    rollback(changes);
+    this.ok(obj1.toString() === cobj1.toString());
+    return this.ok(obj2.toString() === cobj2.toString());
+  });
 
 }).call(this);
